baf9f86eb69888f030e46c1bd2032423edb21740
Marco <marc@MacBook-Pro-de-Marco.local>
2024-08-12 16:18:36 +0200

    Ajout de la sauvegarde

--- liste.py
+++ liste.py
@@ -1,9 +1,25 @@
-### Liste de choses à faire
+### import du module pathlib et indication du chemin du fichier .JSON
+
+from pathlib import Path
+import json
 
 liste = []
+
+chemin = Path("/Users/marc/Documents/python/Mes projets/liste/liste.json")
+if not chemin.exists():
+    print("n'existe pas")
+    with open(chemin, "w") as f:
+        json.dump(liste, f)
+
+else:
+    with open(chemin, "r") as f:
+        liste = json.load(f)
+
+### Liste de choses à faire :
+
 choix_utilisateur = ""
 
-### Boucle True pour demander le choix
+### Boucle True pour demander le choix :
 
 while True:
     choix_utilisateur = input('''Choisissez parmi les 5 options suivantes : 
@@ -11,11 +27,11 @@ while True:
                               2: Retirer un élément de la liste
                               3: Afficher la liste
                               4: Vider la liste
-                              5: Quitter
+                              5: Sauvegarder & Quitter
                               ------------------
                               --> Votre choix : ''')
 
-#### Choix 1 - Ajouter un élément
+#### Choix 1 - Ajouter un élément :
     if choix_utilisateur == "1":
         element = input("Entrez un élément à ajouter à la liste : ")
         if element not in liste:
@@ -24,7 +40,7 @@ while True:
         else:
             print("L'élement est déjà dans la liste")
 
-### Choix 2  - Supprimer un élement
+### Choix 2  - Supprimer un élement :
     elif choix_utilisateur == "2":
         element_supr = input("Quel élément souhaitez vous supprimer ? ")
         if element_supr in liste:
@@ -32,7 +48,7 @@ while True:
         else:
             print("L'élement n'est pas dans la liste")
 
-### Choix 3 - Afficher la liste
+### Choix 3 - Afficher la liste :
     elif choix_utilisateur == "3":
         if liste == []: 
             print("La liste est vide.")
@@ -41,13 +57,17 @@ while True:
                 print(index, i)
                 print("-" * 50)
 
-### Choix 4 - Vider la liste
+### Choix 4 - Vider la liste :
     if choix_utilisateur == "4":
         liste.clear()
         print("La liste a bien été vidée.")
 
-### Choix 5 - Quitter :
+### Choix 5 - Sauvegarder & Quitter :
     if choix_utilisateur == "5":
+        with open(chemin, "w") as f:
+            json.dump(liste,f)
         print("A bientôt")
         break
-    
+
+### - Je sauvegarde le contenu de la liste dans un fichier json :
+
